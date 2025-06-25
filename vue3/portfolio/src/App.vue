47b79e3a7188385563012bd4162d1e5e9dfdfed5
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-06-25 18:22:11 +0900

    feat: vue/section2, 포트폴리오 프로젝트 셋업 vite

---
+++ vue3/portfolio/src/App.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <div class="parent">
+    <h1 class="display-1">App</h1>
+    <button class="btn btn-primary">button</button>
+  </div>
+  <h1>heading 1</h1>
+</template>
+
+<!-- lang 속성이용하여 scss 언어 이용한다고 선언해준다. -->
+<style scoped lang="scss">
+.parent {
+  h1 {
+    color: red;
+  }
+
+  button {
+    color: yellow;
+  }
+}
+</style>
---
+++ vue3/portfolio/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+// bootstrap 불러오기
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+createApp(App).mount("#app");
