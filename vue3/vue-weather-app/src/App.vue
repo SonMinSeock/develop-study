1bfa50cf7275784161f57f345514f3c0499fcf99
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-06-29 22:42:59 +0900

    feat: vue/section3, 프로젝트 셋업

---
+++ vue3/vue-weather-app/src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <h1>App</h1>
+  <!-- 부트스트랩 적용했는지 테스트로 버튼 추가했음 -->
+  <button class="btn btn-primary">홈</button>
+</template>
+
+<style lang="scss" scoped></style>
