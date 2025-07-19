54e88a36745df78b1838a617b779791df2a9030b
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-07-19 19:31:43 +0900

    vue/section4, pinia 스토어에 데이터를 저장하는 방법 part 1

--- vue3/vue-weather-app/src/main.js
+++ vue3/vue-weather-app/src/main.js
@@ -4,11 +4,15 @@ import App from "./App.vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faBarsStaggered, faLocationDot, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import store from "./store/store";
+// import store from "./store/store";
+import { createPinia } from "pinia";
 
 // 사용할 아이콘 불러오기
 
 // 아이콘 등록하기
 library.add(faBarsStaggered, faLocationDot, faMagnifyingGlass);
 
-createApp(App).use(store).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
+// pinia 객체를 생성후 변수에 할당
+const pinia = createPinia();
+
+createApp(App).use(pinia).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
--- vue3/vue-weather-app/src/store/store.js
+++ vue3/vue-weather-app/src/store/store.js
@@ -1,51 +1,12 @@
-import { createStore } from "vuex";
+import { defineStore } from "pinia";
 
-// store 만들기
-export default createStore({
+// store 정의하기
+// main 용 store 정의, 여러 store를 만들수 있다,
+const store = defineStore("main", {
   state: {
-    // initial state
-    count: 0,
-    weatherData: {
-      icon: "icon",
-      temp: 0,
-      text: "text",
-      location: "location",
-      city: "seoul",
-    },
-    toogle: false,
-  },
-  mutations: {
-    // mutataions (데이터 변경)
-    addCount(state, payload) {
-      state.count += 1 + payload;
-    },
-    updateWeather(state, payload) {
-      state.weatherData.icon = payload.weather[0].icon;
-      state.weatherData.temp = payload.main.temp;
-      state.weatherData.text = payload.weather[0].description;
-      state.weatherData.location = payload.sys.country;
-      state.weatherData.city = payload.name;
-    },
-    onSearchCity(state, payload) {
-      state.weatherData.city = payload;
-    },
-    onToggle(state) {
-      state.toggle = !state.toggle;
-    },
+    // 상태 변수 정의
   },
   actions: {
-    // context 전역객체이고 state를 참조하고 있음.
-    getWeather(context) {
-      const API_KEY = import.meta.env.VITE_API_KEY;
-      const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${context.state.weatherData.city}&appid=${API_KEY}`;
-      fetch(API_URL)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          // mutation으로 날씨 정보 업데이트
-          context.commit("updateWeather", data);
-        })
-        .catch("에러가 발생했습니다. 다시 시도해 주세요.");
-    },
+    // 함수 정의
   },
 });
